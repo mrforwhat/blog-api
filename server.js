988df4915542560bc30ff99116471e69c30f1597
gaopenghang <497236467@qq.com>
2019-12-04 16:25:34 +0800

    'init'

---
+++ server.js
@@ -0,0 +1,20 @@
+let http = require('http')
+
+let users = [{
+  id: 1,
+  name: '张三'
+},{
+  id: 2,
+  name: '李四'
+}]
+let server = http.createServer((req,res)=> {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if(req.url === '/api/users') {
+    res.end(JSON.stringify(users))
+  }else {
+    res.end('not found')
+  }
+})
+server.listen(3000,()=> {
+  console.log('后端api服务器已启动在3000端口')
+})
